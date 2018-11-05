deb6e5c76af7d559d2cd286d1eb34d4e913ae740
jeroentvb <1001375@hotmail.nl>
2018-11-05 09:31:43 +0100

    Day five progress

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Hier moet nog een inleiding.
 ## Inhoud
 * [Onderzoeksvragen](#onderzoeksvragen)
 * [Hypothesen](#hypothesen)
+* [Benodigde data](#benodigde-data)
 *
 
 ## Onderzoeksvragen
@@ -21,6 +22,14 @@ Gekozen onderzoeksvraag: *Welk genre heeft gemiddeld genomen het meest aantal pa
 **Alternatieve hypothese:** Er is een genre dat gemiddeld meer pagina's heeft dan anderen.
 
 ## Benodigde data
-* Alle genres (`facets.facet[2].value`)
-* Aantal pagina's van een boeken (`librarian-info.record.marc.df215.df215[0].$t`)
-  * Genre van het boek (`results.result[0].subjects`)
+* Alle genres (`results.result[i].genres.genre[i].search-term` <= array maken van de verkregen genres)
+* Aantal pagina's van een boeken (`results.result[i].librarian-info.record.marc.df215.df215[i].$t`)
+  * Genre van het boek (`results.result[i].genres.genre[i].search-term`)
+
+### Het verkrijgen van genres
+Je kan met een `/search` query filteren op genres. Helaas moet je dan een query meegeven wat er voor zorgt dat je daar op zoekt, en dan de genres er uit filtert.
+De oplossing zou zijn om het `/index` endpoint te gebruiken, want daar hoef je geen query aan mee te geven. Het probleem is echter dat ik geen genres of subjects op kan halen. `Subjects` geeft namelijk geen resultaten terug.
+Om die rede ga ik de genres met de minder precieze `/search` query doen.
+
+## Todo
+* Deelvragen bedenken
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const OBA = require('oba-api')
 const fs = require('fs')
 
@@ -8,7 +10,7 @@ const client = new OBA({
   // ProQuest API Keys
   public: process.env.PUBLIC_KEY,
   secret: process.env.SECRET_KEY
-});
+})
 
 // General usage:
 // client.get({ENDPOINT}, {PARAMS});
@@ -17,40 +19,111 @@ const client = new OBA({
 
 // Client returns a promise which resolves the APIs output in JSON
 
-// Example search to the word 'rijk' sorted by title:
+function getBookPagesAmount (genre, page) {
+  return client.get('search', {
+    q: genre,
+    librarian: true,
+    refine: true,
+    facet: ['type(book)', `genre(${genre})`],
+    page: page
+  })
+    .then(res => {
+      // exportObj('test', res)
+      let data = {
+        meta: {
+          count: Math.ceil((JSON.parse(res)).aquabrowser.meta.count / 20),
+          page: (JSON.parse(res)).aquabrowser.meta.page,
+          genre: genre
+        },
+        pageAmounts: []
+      }
+      let results = (JSON.parse(res)).aquabrowser.results.result
+      results.forEach((item, index) => {
+        let pages = results[index]['librarian-info'].record.marc.df215
+        if (pages) {
+          data.pageAmounts.push(
+            parseInt(pages.df215[0].$t.replace(/(^\d+)(.+$)/i, '$1'))
+          )
+        }
+      })
+      // console.log(data)
+      return data
+    })
+    .catch(err => console.log(err))
+}
+
+// getBookPageAmount('western', 1)
+
+function getData () {
+  let genres = [
+    'western',
+    'thriller',
+    'detective'
+  ]
+  let allPages = []
+
+  genres.forEach(async (item, i) => {
+    let firstRequest = await getBookPagesAmount(genres[i], 1)
+    console.log(`Line 69: ${JSON.stringify(firstRequest)}`)
+    // firstRequest.meta.count.forEach(async (item, x) => {
+    //   // let pages = getBookPagesAmount(genres[i], x + 1)
+    // })
+  })
+}
+
+getData()
+/*
 client.get('search', {
-  q: 'Arendsoog',
-  // sort: 'title',
+  q: 'b',
+  // sort: 'title', // Soirt results on title (or something else)
   librarian: true,
-  // pagesize: 2,
+  page: 1,
+  // pagesize: 2, // Amount of results per page
   refine: true,
-  facet: 'type(book)'
+  facet: ['type(book)', 'genre(western)']
 })
   .then(res => {
-    exportObj('test', res)
-    var array = []
-    var data = (JSON.parse(res)).aquabrowser.results.result
-    // console.log(data)
-    // aquabrowser.results.result[0].titles.title.$t
+    // exportObj('test', res)
+    // let data = []
+    let pageAmounts = []
+    // [
+    //   {
+    //     name: 'western',
+    //     averagePageAmount: Number
+    //   }
+    // ]
+    let results = (JSON.parse(res)).aquabrowser.results.result // All the results
+    // let allGenres = (JSON.parse(res)).aquabrowser.facets.facet[2].value // Genres in current query
+    // let genre = results[0].genres.genre[0]['search-term']
+    // console.log(genre)
+    // console.log(allGenres)
+    // let pageCount = results[0]['librarian-info'].record.marc.df215.df215[0].$t // Get the amount of pages in a book
+    // console.log(pageCount)
 
-    data.forEach(function (item, index) {
-      console.log(data[index].titles.title.$t)
-      array.push(data[index].titles.title.$t)
+    results.forEach((item, index) => {
+      pageAmounts.push(
+        parseInt(results[index]['librarian-info'].record.marc.df215.df215[0].$t.split(' ')[0]) // Get all page lengths
+      )
     })
-    
-    console.log(array)
+    // console.log(genre)
+    // console.log(pageAmounts)
 
+    let total = 0
+    pageAmounts.forEach((item, index) => { // Get the total pages in a genre
+      total += pageAmounts[index]
+    })
+    total = Math.round(total / pageAmounts.length)
+    console.log(total)
   }) // JSON results
   .catch(err => console.log(err)) // Something went wrong in the request to the API
+*/
+function formatJson (obj) {
+  return JSON.stringify(JSON.parse(obj), null, 4)
+}
 
-
-  function formatJson(obj) {
-    return JSON.stringify(JSON.parse(obj), null, 4)
-  }
-
-  function exportObj(name, obj) {
-    fs.writeFile(name + '-export.json', formatJson(obj), err => {
-      if (err) throw err
-      console.log(`${name}-export.json written`)
-    })
-  }
+function exportObj (name, obj) {
+  fs.writeFile(name + '-export.json', formatJson(obj), err => {
+    if (err) throw err
+    console.log(`${name}-export.json written`)
+  })
+}
